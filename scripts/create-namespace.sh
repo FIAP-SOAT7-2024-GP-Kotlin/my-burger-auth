9dc96b7010b41d824a130cd2e5ce47f5e58239a1
Luiz Fernando Calazans <luiz.calazans@ifood.com.br>
2024-09-19 09:06:55 -0300

    Update function

--- scripts/create-namespace.sh
+++ scripts/create-namespace.sh
@@ -3,4 +3,5 @@ PROJECT_ID=1f3e6919-579f-400b-9dab-a6dafaaaafa7
 PROJECT_NAME=first-project
 NAMESPACE_NAME=authentication-namespace
 
+echo "Creating namespace - $NAMESPACE_NAME"
 doctl serverless namespaces create --label $NAMESPACE_NAME --region "nyc1"
